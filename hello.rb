4df10ac2d4c8e25095f28d4bf0ed8ab6a400532a
Alif <alifraher@gmail.com>
2021-12-17 07:52:13 +0700

    Shows content of the highest number of files that have the same content

--- hello.rb
+++ hello.rb
@@ -3,9 +3,20 @@ dir = gets.chomp()
 
 directories = Dir.glob(dir + "/**/*").reject { |f| File.directory?(f) }
 
-arr = Array.new
+if(directories.length > 0)
+    arr = Array.new
 
-for file in directories
-    readFile = File.open(file, "r")
-    arr.push(readFile.read())
+    for file in directories
+        readFile = File.open(file, "r")
+        arr.push(readFile.read())
+    end
+
+    hash = Hash.new(0)
+    arr.each { | v | hash.store(v, hash[v]+1) }
+
+    result = hash.max_by{|k,v| v}
+
+    puts "#{result[0]} #{result[1]}"
+else
+    puts "Directory doesn't exist or empty"
 end
