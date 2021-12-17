7ee2437fa7677b7ea15b5205231ef325a5994c80
Alif <alifraher@gmail.com>
2021-12-17 07:25:35 +0700

    Insert all content of files to array

--- hello.rb
+++ hello.rb
@@ -1,1 +1,8 @@
 directories = Dir.glob("DropsuiteTest/**/*").reject { |f| File.directory?(f) }
+
+arr = Array.new
+
+for file in directories
+    readFile = File.open(file, "r")
+    arr.push(readFile.read())
+end
